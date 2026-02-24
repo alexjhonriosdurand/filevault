0e41fb0da7063fef510e87801b764bfd43f2fc62
alexjhonriosdurand <alexjhonriosdurand@gmail.com>
2026-02-24 10:50:23 -0500

    Create main.dart

---
+++ lib/main.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+import 'screens/home_screen.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'FileVault',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(
+          seedColor: const Color(0xFF00C853),
+          brightness: Brightness.dark,
+        ),
+        scaffoldBackgroundColor: const Color(0xFF0A0A0A),
+        useMaterial3: true,
+      ),
+      home: const HomeScreen(),
+    );
+  }
+}
